32a6b28e14365e59ee5a9d7016e53460ca98e9b3
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-28 23:59:52 +0530

    input ouput update of data in array

---
+++ Array/inputoutput.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class inputoutputupdate{
+    public static void main(String[]args){
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        // int maths
+        // maths = sc.nextInt();
+    
+     marks[0] = sc.nextInt();  //maths
+     marks[1] = sc.nextInt();  //phy
+     marks[2] = sc.nextInt();  //chem
+    
+     System.out.println("maths: " +marks[0]);  //maths
+     System.out.println("phy: " +marks[1]);  //phy
+     System.out.println("chem: " +marks[2]);  //chem
+
+     //if  we want to update (mark is wrong)then
+
+     marks[1] = marks[1] +2;
+     System.out.println("phy: " +marks[1]);
+
+    }
+}
