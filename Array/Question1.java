82d1d5e50983fda5dfe379470f49eafa72f79017
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-29 00:10:01 +0530

    percentage using array

---
+++ Array/Question1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Question1{
+    public static void main(String[]args){
+        int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+
+        // int maths
+        // maths = sc.nextInt();
+    
+     marks[0] = sc.nextInt();  //maths
+     marks[1] = sc.nextInt();  //phy
+     marks[2] = sc.nextInt();  //chem
+    
+     System.out.println("maths: " +marks[0]);  //maths
+     System.out.println("phy: " +marks[1]);  //phy
+     System.out.println("chem: " +marks[2]);  //chem
+
+     int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+     System.out.println("Percentage = " +percentage + "%");
+    }
+}
