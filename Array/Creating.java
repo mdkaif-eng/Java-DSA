754ce01b296a5e505beb7116794338503b84249a
Md Kaif <mdkaif.kmclu@gmail.com>
2025-12-28 23:49:56 +0530

    Creation of array

---
+++ Array/Creating.java
@@ -0,0 +1,8 @@
+public class Creating {
+    public static void main(String[]args){
+        // Creating an array
+
+        int marks[] = new int[50];
+        int numbers[] = {1, 2, 3}; 
+    }
+}
