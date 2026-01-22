0c8850ebd83a45595c0cf5337e3a5d8685587c93
Md Kaif <mdkaif.kmclu@gmail.com>
2026-01-22 23:06:58 +0530

    linear search of key index

--- arrayArgument.java
+++ arrayArgument.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class arrayArgument{
     public static void update (int marks[]){
         for(int i=0; i<marks.length; i++){
-            marks[i] = marks[i] + 1;
+            marks[i] = marks[i] + 1;     
         }
     }
     public static void main(String[]args){
