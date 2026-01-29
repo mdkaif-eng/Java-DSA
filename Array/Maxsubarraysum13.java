074901f068ceea609ae1218005c377ee8a31acfe
Md Kaif <mdkaif.kmclu@gmail.com>
2026-01-29 16:04:09 +0530

    sum of prefix sub array

---
+++ Array/Maxsubarraysum13.java
@@ -0,0 +1,41 @@
+//sum of prefix array
+    import java.util.*;
+public class Maxsubarraysum13 {
+     public static void maxsubarraySum(int numbers[]){
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int [numbers.length];
+        prefix[0] = numbers[0];
+        
+        //calculate sum of aaray
+        for(int i = 1; i < prefix.length; i++){
+            prefix[i] = prefix[i-1] + numbers[i];
+        }
+
+        for(int i=0; i<numbers.length; i++){
+            int start = i;
+            for(int j=i; j<numbers.length; j++){
+                int end = j;
+                currSum = start == 0 ? prefix[end] : prefix[end] - prefix[start-1];
+
+                //for(int k=start; k<=end; k++){  //print
+               //subarray sum 
+              //   currSum += numbers[k];
+             //   }
+                 
+                if(maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+            System.out.println("max sum of subarrays :" + maxSum);
+
+     }
+
+    public static void main (String args[]){
+        int numbers[] = {1,-2,6,-1,3};
+        maxsubarraySum(numbers);
+    }
+}
+    
+
