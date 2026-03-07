7d66e43fdc5f66c3c100562b9a0051569f2e1d79
Md Kaif <mdkaif.kmclu@gmail.com>
2026-03-07 15:38:15 +0530

    if we want only starting index of 0 and ending index 3 and rest of same

---
+++ Basic Sorting Algorithm/InbuiltSort.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+public class InbuiltSort {
+    public static void printArr(int arr[]) {
+
+    for(int i = 0; i<arr.length; i++){
+        System.out.print(arr[i]+"");
+    }
+        System.out.println();
+  }
+    public static void selectionSort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++){
+            int minPos = i;
+            for(int j = i+1; j<arr.length; j++){
+                if(arr[minPos] > arr[j]){
+                    minPos = j;
+                }
+            }
+            // swap 
+            int temp = arr[minPos];
+            arr[minPos] = arr[i];
+            arr[i] = temp;
+            printArr(arr);
+        }   
+    }
+ public static void main(String[] args) {
+        int arr[] = {5, 4, 1,6, 3, 2};
+       // selectionSort(arr);
+       Arrays.sort(arr,0,3);
+
+        printArr(arr);
+    }
+}
+    
+
